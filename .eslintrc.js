b77393afad63d6ffa91b4e966adf9e247c6f2d93
FlyDreame <onedreamcat@gmail.com>
2018-04-17 17:31:53 +0800

    初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const version = require('./package.json').version
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    'plugin:eslint-plugin/recommended',
+    'plugin:vue-libs/recommended'
+  ],
+  plugins: [
+    'eslint-plugin'
+  ],
+  rules: {
+    'eslint-plugin/report-message-format': ['error', '^[A-Z`\'].*\\.$'],
+    'eslint-plugin/prefer-placeholders': 'error',
+    'eslint-plugin/consistent-output': 'error'
+  },
+
+  overrides: [{
+    files: ['lib/rules/*.js'],
+    rules: {
+      "consistent-docs-description": "error",
+      "no-invalid-meta": "error"
+    }
+  }]
+}
